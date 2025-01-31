6c31a0da9337fc252e3c4411ee69cd657b9b2e51
chimdienn <156086389+chimdienn@users.noreply.github.com>
2025-02-01 01:45:38 +1100

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  count = 0;
   res.render("index.ejs", {currentYear: year});
 });
 
